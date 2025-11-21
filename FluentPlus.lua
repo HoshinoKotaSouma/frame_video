a1868c8623c4dafb31abe6b149fb9dfcc3829382
@BaconnRinn <kiluane12@gmail.com>
2025-11-21 10:56:19 +0700

    Update FluentPlus.lua

--- FluentPlus.lua
+++ FluentPlus.lua
@@ -39,6 +39,7 @@ local ProtectGui = protectgui or (syn and syn.protect_gui) or function() end
 local Themes = {
 	Names = {
 		"Dark",
+		"Anime",
 		"Darker", 
 		"AMOLED",
 		"Light",
@@ -95,6 +96,55 @@ local Themes = {
 		Hover = Color3.fromRGB(120, 120, 120),
 		HoverChange = 0.07,
 	},
+	    Anime = {
+        Name = "Anime",
+        Accent = Color3.fromRGB(96, 205, 255),
+        AcrylicMain = Color3.fromRGB(60, 60, 60),
+        AcrylicBorder = Color3.fromRGB(90, 90, 90),
+        AcrylicGradient = ColorSequence.new(Color3.fromRGB(40, 40, 40), Color3.fromRGB(40, 40, 40)),
+        AcrylicNoise = 0.9,
+        TitleBarLine = Color3.fromRGB(75, 75, 75),
+        Tab = Color3.fromRGB(120, 120, 120),
+        Element = Color3.fromRGB(120, 120, 120),
+        ElementBorder = Color3.fromRGB(35, 35, 35),
+        InElementBorder = Color3.fromRGB(90, 90, 90),
+        ElementTransparency = 0.87,
+        ToggleSlider = Color3.fromRGB(120, 120, 120),
+        ToggleToggled = Color3.fromRGB(42, 42, 42),
+        SliderRail = Color3.fromRGB(120, 120, 120),
+        DropdownFrame = Color3.fromRGB(160, 160, 160),
+        DropdownHolder = Color3.fromRGB(45, 45, 45),
+        DropdownBorder = Color3.fromRGB(35, 35, 35),
+        DropdownOption = Color3.fromRGB(120, 120, 120),
+        Keybind = Color3.fromRGB(120, 120, 120),
+        Input = Color3.fromRGB(160, 160, 160),
+        InputFocused = Color3.fromRGB(10, 10, 10),
+        InputIndicator = Color3.fromRGB(150, 150, 150),
+        Dialog = Color3.fromRGB(45, 45, 45),
+        DialogHolder = Color3.fromRGB(35, 35, 35),
+        DialogHolderLine = Color3.fromRGB(30, 30, 30),
+        DialogButton = Color3.fromRGB(45, 45, 45),
+        DialogButtonBorder = Color3.fromRGB(80, 80, 80),
+        DialogBorder = Color3.fromRGB(70, 70, 70),
+        DialogInput = Color3.fromRGB(55, 55, 55),
+        DialogInputLine = Color3.fromRGB(160, 160, 160),
+        Text = Color3.fromRGB(240, 240, 240),
+        SubText = Color3.fromRGB(170, 170, 170),
+        Hover = Color3.fromRGB(120, 120, 120),
+        HoverChange = 0.07,
+
+        -- GIF frames
+        Frames = (function()
+            local t = {}
+            for i = 0, 49 do
+                t[i+1] = string.format(
+                    "https://raw.githubusercontent.com/HoshinoKotaSouma/frame_video/main/frame_%02d_delay-0.1s.jpg",
+                    i
+                )
+            end
+            return t
+        end)()
+    },
 	Darker = {
 		Name = "Darker",
 		Accent = Color3.fromRGB(56, 109, 223),
@@ -11091,4 +11141,5 @@ end)
     
 
 if RunService:IsStudio() then task.wait(0.01) end
+
 return Library, SaveManager, InterfaceManager, Mobile
